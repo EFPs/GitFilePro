2762f3e042414d1628b7f908462c43ceaa79c557
EFPs <st_18912@hotmail.com>
2017-10-05 00:13:43 +0700

    Modified awesome.sh, restore.sh

--- a1/awesome.sh
+++ a1/awesome.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 #$1 $2 ...$NF
 k=$1
-if [$k -eq $k 2>/del/null ]
+intCheck="$k |grep '/[0-9]+'"
+
+if [[ $k =~ ^-?[0-9]+$ ]]
 then
   names=`ls -la|awk -v k="$k" '$5 > k {print $NF}'`
+  if [[ $names == "" ]]
+  then
+    echo "No file/folder that are larger than $k bytes."
+  fi
   echo "${names[*]}"
 else
-  echo "Wrong Argument"
-  echo "type --help for help."
+  if [ $k == "--help" ]
+  then
+    echo "awesome.sh takes argument k, and return all directories that are larger than k bytes."
+  else
+    echo "Wrong Argument"
+    echo "type awesome.sh --help for help."
+  fi
 fi
