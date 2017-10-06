33a79994249a136190db303e7e67cae7efa19950
EFPs <st_18912@hotmail.com>
2017-10-06 19:28:46 +0700

    Add if condition for empty/invalid argument.

--- a1/backup.sh
+++ a1/backup.sh
@@ -2,7 +2,17 @@
 
 file=$1
 
+if [ -z "$file" ];then
+  echo "Please enter a directory of a folder with out / at the end."
+  exit
+fi
 
+if [ ! -d "$file/" ];
+  then
+    echo "No such directory"
+    echo "Please enter a valid directory."
+    exit
+fi
 
 time="_"`date -u +%F_%T`
 fileName=`basename $file`
@@ -10,13 +20,9 @@ fileName=`basename $file`
 #echo `mkdir -p /subm/u5881050/backups`
 
 echo `mkdir -p /subm/u5881050/backups/$fileName$time`
-num=`cat $file|grep '.'`
-if [[ num=="" ]]
-then
-  echo `cp -r $file/* /subm/u5881050/backups/$fileName$time`
-else
-  echo `cp -r $file /subm/u5881050/backups/$fileName$time`
+echo `cp -R $file/ /subm/u5881050/backups/$fileName$time`
+
+echo "Backup $file completed successfully."
 
 
 #echo "$B"
-echo "Backup $file completed successfully."
